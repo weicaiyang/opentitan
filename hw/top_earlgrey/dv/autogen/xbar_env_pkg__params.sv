1d05cf800f968e0949c1e8f1f524e4181403b8c9
Weicai Yang <weicai@google.com>
2021-06-25 11:33:36 -0700

    [dv] Fix chip xbar failures
    Signed-off-by: Weicai Yang <weicai@google.com>

--- hw/top_earlgrey/dv/autogen/xbar_env_pkg__params.sv
+++ hw/top_earlgrey/dv/autogen/xbar_env_pkg__params.sv
@@ -155,16 +155,16 @@ tl_device_t xbar_devices[$] = '{
   // List of Xbar hosts
 tl_host_t xbar_hosts[$] = '{
     '{"corei", 0, '{
-        "rom_ctrl.rom",
-        "rv_dm.rom",
+        "rom_ctrl__rom",
+        "rv_dm__rom",
         "ram_main",
         "eflash"}}
     ,
     '{"cored", 1, '{
-        "rom_ctrl.rom",
-        "rom_ctrl.regs",
-        "rv_dm.rom",
-        "rv_dm.regs",
+        "rom_ctrl__rom",
+        "rom_ctrl__regs",
+        "rv_dm__rom",
+        "rv_dm__regs",
         "ram_main",
         "eflash",
         "uart0",
@@ -196,8 +196,8 @@ tl_host_t xbar_hosts[$] = '{
         "adc_ctrl_aon",
         "sysrst_ctrl_aon",
         "pwm_aon",
-        "flash_ctrl.core",
-        "flash_ctrl.prim",
+        "flash_ctrl__core",
+        "flash_ctrl__prim",
         "aes",
         "entropy_src",
         "csrng",
@@ -210,10 +210,10 @@ tl_host_t xbar_hosts[$] = '{
         "kmac",
         "sram_ctrl_main"}}
     ,
-    '{"rv_dm.sba", 2, '{
-        "rom_ctrl.rom",
-        "rom_ctrl.regs",
-        "rv_dm.regs",
+    '{"rv_dm__sba", 2, '{
+        "rom_ctrl__rom",
+        "rom_ctrl__regs",
+        "rv_dm__regs",
         "ram_main",
         "eflash",
         "uart0",
@@ -245,8 +245,8 @@ tl_host_t xbar_hosts[$] = '{
         "adc_ctrl_aon",
         "sysrst_ctrl_aon",
         "pwm_aon",
-        "flash_ctrl.core",
-        "flash_ctrl.prim",
+        "flash_ctrl__core",
+        "flash_ctrl__prim",
         "aes",
         "entropy_src",
         "csrng",
